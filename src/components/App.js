2fe6b3e6d0d675dc29af738d4d4a2f8efd262a75
ShaikhShehroz <imshaikh010@gmail.com>
2019-12-13 00:27:13 +0530

    for checking purpose

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import StreamList from '../streams/StreamList';
 import StreamCreate from '../streams/StreamCreate';
 import StreamDelete from '../streams/StreamDelete';
